f218a6b1e92ed785e688d7b80bf335f15d380748
huleitao <leitao0906@qq.com>
2026-07-28 10:32:21 +0800

    fix: 移除掉不需要的异步线程池

--- db-genius-web/src/main/java/com/dbgenius/DbGeniusApplication.java
+++ db-genius-web/src/main/java/com/dbgenius/DbGeniusApplication.java
@@ -3,11 +3,9 @@ package com.dbgenius;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @MapperScan("com.dbgenius.mapper")
-@EnableAsync
 public class DbGeniusApplication {
 
     public static void main(String[] args) {
